c008ba88b1fe14034dbd874bbc533648b1dfdf4d
Anton <santon84@gmail.com>
2023-01-18 22:56:09 +0300

    sorter

--- src/pages/Orders/OrdersHeader.jsx
+++ src/pages/Orders/OrdersHeader.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 function OrdersHeader() {
   return (
-    <div>
+    <div className='orders__table-header'>
       <h3>Заказы</h3>
+      <select>
+        <option value={2}>По дате</option>
+        <option value={1}>Сумме</option>
+        <option value={1}>Номеру заказа</option>
+      </select>
     </div>
   )
 }
